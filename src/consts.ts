b6b98ac931111ee1e72b47e536d6d2696e650919
ClasWen <clas.wen@icloud.com>
2024-03-30 14:20:38 +0800

    init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = `计算机协会`;
+export const SITE_EMAIL = 'contact@nbtca.space'
+export const SITE_NAME = 'Computer Association';
+export const SITE_DESCRIPTION = 'Computer Association';
+export const SITE_URL = "https://www.nbtca.space";
