8c9dde0f6896d5ac2965738c0d35c5383a095da0
lyq <1661898579@qq.com>
2024-04-25 22:28:42 +0800

    update about page

---
+++ src/data/otherMembers.ts
@@ -0,0 +1,41 @@
+interface Member {
+  memberId: string
+  alias: string
+  avatar: string
+  profile: string
+}
+
+const otherMembers: Member[] = [
+  {
+    memberId: undefined,
+    alias: "鲁冠泽",
+    profile: "Java, Web。",
+    avatar: "",
+  },
+  {
+    memberId: undefined,
+    alias: "江蕾",
+    profile: "前端。",
+    avatar: "",
+  },
+  {
+    memberId: undefined,
+    alias: "黄文轩",
+    profile: "网安, Linux和C/C++。",
+    avatar: "",
+  },
+  {
+    memberId: undefined,
+    alias: "陈学书",
+    profile: "Mac, 人工智能, Web和流水账。",
+    avatar: "",
+  },
+  {
+    memberId: undefined,
+    alias: "章晟玮",
+    profile: "算法记录。",
+    avatar: "",
+  },
+]
+
+export default otherMembers
