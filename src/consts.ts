0639315e8fa7e488b07af448a4192cb885faabf4
ClasWen <clas.wen@icloud.com>
2024-06-03 21:49:59 +0800

    fix for compliance

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `计算机协会`
+export const SITE_TITLE = `拔电关机`
 export const SITE_EMAIL = "contact@nbtca.space"
 export const SITE_NAME = "Computer Association"
 export const SITE_DESCRIPTION = "Computer Association"
