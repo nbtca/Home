8f83729125c142c5fc908e0229c6ac8718de0ba4
LazuliKao <LazuliKao233@outlook.com>
2024-06-17 11:32:36 +0800

    使用昵称

--- src/store/member.ts
+++ src/store/member.ts
@@ -1,5 +1,5 @@
 export interface Member {
-  memberId: string
+  memberId?: string
   alias: string
   avatar: Promise<typeof import("*.jpg")> | string
   profile: string
@@ -8,43 +8,37 @@ export interface Member {
 
 export const otherMembers: Member[] = [
   {
-    memberId: undefined,
-    alias: "鲁冠泽",
+    alias: "cimoc",
     profile: "Java, Web。",
     avatar: import("../pages/posts/blogs/更多/assets/avatar/cimoccn.png"),
     link: "https://cimoc.cn/",
   },
   {
-    memberId: undefined,
-    alias: "江蕾",
+    alias: "J1ay",
     profile: "前端。",
     avatar: import("../pages/posts/blogs/更多/assets/avatar/DoEH51Nj97Ah64a.png"),
     link: "https://www.cnblogs.com/JLay",
   },
   {
-    memberId: undefined,
-    alias: "黄文轩",
+    alias: "N3ptune",
     profile: "网安, Linux和C/C++。",
     avatar: import("../pages/posts/blogs/更多/assets/avatar/4J9NfH1UZD3sz5I.png"),
     link: "https://www.cnblogs.com/N3ptune",
   },
   {
-    memberId: undefined,
-    alias: "陈学书",
+    alias: "Flat-White",
     profile: "Mac, 人工智能, Web和流水账。",
     avatar: import("../pages/posts/blogs/更多/assets/avatar/VjBGkQ6c58vH4l9.png"),
     link: "https://www.cnblogs.com/Flat-White",
   },
   {
-    memberId: undefined,
-    alias: "王纯",
+    alias: "Chun.",
     profile: "Web。",
     avatar: import("../pages/posts/blogs/更多/assets/avatar/avatar.png"),
     link: "https://chundot.org",
   },
   {
-    memberId: undefined,
-    alias: "章晟玮",
+    alias: "bcscb",
     profile: "算法记录。",
     avatar: undefined,
     link: "https://bcscb.xyz/",
