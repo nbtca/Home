d34eb7dba46c64cfaa2694c5fc6c1cd776466bfe
LazuliKao <LazuliKao233@outlook.com>
2024-06-18 13:44:23 +0800

    edit members

--- src/store/member.ts
+++ src/store/member.ts
@@ -6,14 +6,13 @@ export interface Member {
   link?: string
 }
 
-export const otherMembers: Member & { year: number }[] = [
-  {
-    year: 2021,
-    alias: "cimoc",
-    profile: "Java, Web。",
-    avatar: import("../pages/posts/blogs/更多/assets/avatar/cimoccn.png"),
-    link: "https://cimoc.cn/",
-  },
+export const otherMembers: (Member & { year?: number })[] = [
+  // {
+  //   alias: "cimoc",
+  //   profile: "Java, Web。",
+  //   avatar: import("../pages/posts/blogs/更多/assets/avatar/cimoccn.png"),
+  //   link: "https://cimoc.cn/",
+  // },
   {
     alias: "J1ay",
     profile: "前端。",
@@ -26,24 +25,24 @@ export const otherMembers: Member & { year: number }[] = [
     avatar: import("../pages/posts/blogs/更多/assets/avatar/4J9NfH1UZD3sz5I.png"),
     link: "https://www.cnblogs.com/N3ptune",
   },
-  {
-    alias: "双份浓缩馥芮白",
-    profile: "Mac, 人工智能, Web和流水账。",
-    avatar: import("../pages/posts/blogs/更多/assets/avatar/44250c1b6e2f243ff60949efe9269348.png"),
-    link: undefined,
-  },
+  // {
+  //   alias: "双份浓缩馥芮白",
+  //   profile: "Mac, 人工智能, Web和流水账。",
+  //   avatar: import("../pages/posts/blogs/更多/assets/avatar/44250c1b6e2f243ff60949efe9269348.png"),
+  //   link: undefined,
+  // },
   {
     alias: "Chun.",
     profile: "Web。",
     avatar: import("../pages/posts/blogs/更多/assets/avatar/avatar.png"),
     link: undefined, // "https://chundot.org",
   },
-  {
-    alias: "bcscb",
-    profile: "算法记录。",
-    avatar: undefined,
-    link: undefined, // "https://bcscb.xyz/",
-  },
+  // {
+  //   alias: "bcscb",
+  //   profile: "算法记录。",
+  //   avatar: undefined,
+  //   link: undefined, // "https://bcscb.xyz/",
+  // },
 ]
 // export const memberWithLink: Member[] = [
 // https://zzh0u.github.io/
