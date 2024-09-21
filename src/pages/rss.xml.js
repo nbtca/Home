b25c0701d625d45e493bac7bd951fa23b42b2b18
LazuliKao <LazuliKao233@outlook.com>
2024-09-21 15:42:27 +0800

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss"
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: "https://astro-blog.qum.cc",
+    title: "NBTCA Home",
+    description: "NBTCA Home Page",
+    site: "https://www.nbtca.space",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>zh-cn</language>`,
   })
