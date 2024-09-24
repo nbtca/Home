f6c10df351bd481bf709433185e704ba68d9854b
LazuliKao <LazuliKao233@outlook.com>
2024-09-24 16:54:16 +0800

    update client

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,10 @@
 import createClient from "openapi-fetch"
-import type { paths } from "../types/saturday"
+import type { paths as saturdayPaths } from "../types/saturday"
+import type { paths as activePaths } from "../types/active"
 
-export const saturdayClient = createClient<paths>({
+export const saturdayClient = createClient<saturdayPaths>({
   baseUrl: "https://api.nbtca.space/v2/",
+})
+export const activeClient = createClient<activePaths>({
+  baseUrl: "https://active.nbtca.space/",
 })
