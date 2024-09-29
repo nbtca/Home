3b55f892b960391f3dfbcaea2120369b2e843c4b
ClasWen <clas.wen@icloud.com>
2024-09-29 22:47:16 +0800

    organize files

---
+++ src/pages/join-us/list.tsx
@@ -0,0 +1,11 @@
+import BaseLayout from "../../layouts/BaseLayout.astro"
+
+export default function JoinUsList() {
+  return (
+    <BaseLayout>
+      <div>
+        <h1>Join Us List</h1>
+      </div>
+    </BaseLayout>
+  )
+}
