da0566143e9e3cda4da5c83a319cad5ce9e9850d
Clas Wen <clas.wen@icloud.com>
2025-01-28 15:13:48 +0800

    fix dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,7 @@
 import { nextui } from "@nextui-org/react"
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -12,6 +9,6 @@ export default {
       },
     },
   },
-  darkMode: "class",
+  darkMode: ["selector", "body.theme-dark"],
   plugins: [nextui()],
 }
