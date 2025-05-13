d782137f81de3d331e577f0f551fb72f89162087
Clas Wen <clas.wen@icloud.com>
2025-05-13 22:00:08 +0800

    save

--- src/pages/repair/TicketForm.tsx
+++ src/pages/repair/TicketForm.tsx
@@ -3,49 +3,28 @@ import { makeLogtoClient } from "../../utils/auth"
 import type { UserInfoResponse } from "@logto/browser"
 import { Form, Input, Button, Textarea } from "@heroui/react"
 import { saturdayClient } from "../../utils/client"
-import type LogtoClient from "@logto/browser"
+import type { components } from "../../types/saturday"
+import QRCode from "qrcode"
+import EventDetail from "./EventDetail"
 
-export default function App() {
-  const [userInfo, setUserInfo] = useState<UserInfoResponse>()
-  const [formData, setFormData] = useState<{
-    model?: string
-    phone?: string
-    qq?: string
-    description?: string
-  }>({})
+type TicketFormData = {
+  model?: string
+  phone?: string
+  qq?: string
+  description?: string
+}
 
-  let logtoClient: LogtoClient | undefined = undefined
-
-  useEffect(() => {
-    const check = async () => {
-      logtoClient = makeLogtoClient()
-      const createRepairPath = "/repair/create-ticket"
-      const authenticated = await logtoClient.isAuthenticated()
-      if (!authenticated) {
-        window.location.href = `/repair/login-hint?redirectUrl=${createRepairPath}`
-        return
-      }
-      const res = await logtoClient.getIdTokenClaims()
-      setUserInfo(res)
-    }
-    check()
-  }, [])
-
+function TicketForm(props: {
+  userInfo: UserInfoResponse
+  onSubmit: (form: TicketFormData) => Promise<void>
+}) {
+  const [loading, setLoading] = useState<boolean>()
+  const [formData, setFormData] = useState<TicketFormData>({})
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
-    const logtoToken = await logtoClient.getAccessToken()
-    await saturdayClient.POST("/client/event", {
-      headers: {
-        Authorization: `Bearer ${logtoToken}`,
-      },
-      body: {
-        Problem: formData.description,
-        model: formData.model,
-        phone: formData.phone,
-        qq: formData.qq,
-      },
-    })
+    setLoading(true)
+    await props.onSubmit(formData)
+    setLoading(false)
   }
 
   return (
@@ -96,7 +75,7 @@ export default function App() {
             placeholder="example@nbtca.space"
             isRequired
             type="email"
-            value={userInfo?.email || ""}
+            value={props.userInfo?.email || ""}
             readOnly
             description="我们会向此邮箱发送维修相关的通知"
           />
@@ -108,6 +87,7 @@ export default function App() {
             type="phone"
             name="phone"
             value={formData.phone || ""}
+            maxLength={11}
             onChange={(e) => {
               setFormData({ ...formData, phone: e.target.value })
             }}
@@ -120,11 +100,115 @@ export default function App() {
               setFormData({ ...formData, qq: e.target.value })
             }}
           />
-          <Button type="submit" color="primary" className="my-4 w-full">
+          <Button type="submit" color="primary" className="my-4 w-full" isLoading={loading}>
             提交
           </Button>
         </Form>
       </div>
     </section>
+  )
+}
+type PublicEvent = components["schemas"]["PublicEvent"]
+function TicketFormCreated(props: {
+  event: PublicEvent
+}) {
+  const [svg, setSvg] = useState<string>()
+
+  useEffect(() => {
+    QRCode.toString(props.event.eventId.toString()).then((res) => {
+      // const root = createRoot(
+      //   document.getElementById("svg-root"),
+      // )
+      // root.render(res)
+      setSvg(res)
+    })
+  })
+  // const svg = ref()
+  // watch(id, async () => {
+  //   if (!id.value) {
+  //     return
+  //   }
+  //   svg.value = await QRCode.toString(useGraduationIdURL().constructURL(id.value))
+  // })
+
+  return (
+    <div className="w-full h-[80vh] flex flex-col justify-between items-center">
+      <div className=" flex flex-col sm:flex-row items-center h-full max-w-[1280px] justify-center gap-8 p-8">
+        <div className="min-h-64 h-[36vh] aspect-square">
+          <div className="h-full" dangerouslySetInnerHTML={{ __html: svg }}></div>
+        </div>
+        <div
+          className=" sm:w-auto"
+        >
+          <div className="flex items-center gap-2">
+            <div className="text-brand text-nowrap text-3xl font-bold">
+              预约成功
+            </div>
+          </div>
+          <div className="sm:w-[30vw] mt-6 text-gray-600 lg:text-lg">
+            <div>
+              扫描二维码来查看预约详情
+            </div>
+            <div>
+              你也可以保存此二维码
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    // <div>{props.event.eventId}</div>
+  )
+}
+
+export default function App() {
+  const [userInfo, setUserInfo] = useState<UserInfoResponse>()
+  const [event, setEvent] = useState<PublicEvent | undefined>({
+    eventId: 123,
+  })
+
+  useEffect(() => {
+    const check = async () => {
+      const createRepairPath = "/repair/create-ticket"
+      const authenticated = await makeLogtoClient().isAuthenticated()
+      if (!authenticated) {
+        window.location.href = `/repair/login-hint?redirectUrl=${createRepairPath}`
+        return
+      }
+      const res = await makeLogtoClient().getIdTokenClaims()
+      setUserInfo(res)
+    }
+    check()
+  }, [])
+
+  const onSubmit = async (formData: TicketFormData) => {
+    const logtoToken = await makeLogtoClient().getAccessToken()
+    try {
+      const res = await saturdayClient.POST("/client/event", {
+        headers: {
+          Authorization: `Bearer ${logtoToken}`,
+        },
+        body: {
+          Problem: formData.description,
+          model: formData.model,
+          phone: formData.phone,
+          qq: formData.qq,
+        },
+      })
+      setEvent(res.data as unknown as PublicEvent)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <EventDetail />
+      (
+      event?.eventId
+      ? <TicketFormCreated event={event}></TicketFormCreated>
+      : <TicketForm userInfo={userInfo} onSubmit={onSubmit}></TicketForm>
+      )
+    </>
   )
 }
