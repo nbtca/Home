425d0c4da496650e1bcba841e4d5adaf86b3fd57
Clas Wen <clas.wen@icloud.com>
2025-05-24 21:58:19 +0800

    update style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `拔电关机`
 export const SITE_EMAIL = "contact@nbtca.space"
-export const SITE_NAME = "Computer Association"
+export const SITE_NAME = "NingboTech University, Computer Association"
 export const SITE_DESCRIPTION = "Computer Association"
 export const SITE_URL = import.meta.env.PUBLIC_SITE_URL
